a96ab52afd79fa4e7d7c54f702bd7391dcd35137
JameFLC <jame.flochlecarour@gmail.com>
2022-12-29 20:13:00 +0100

    Fixed image input description on mobile

--- sources/ts/components/image-input.ts
+++ sources/ts/components/image-input.ts
@@ -47,8 +47,8 @@ class ImageInput extends HTMLElement {
             <div class="image-input-box" id="image-box">
                 <input id="image-input" type="file" name="" accept="image/*" class="hidden">
                 <label for="image-input" title="Image file input. Drop an image or click here.">
-                  <img id="display-image"src="./assets/images/transparent-pixel.png" alt="">
                   <p class="image-description">Drop an image or click here !</p>
+                  <img id="display-image"src="./assets/images/transparent-pixel.png" alt="">
                 </label>
                 <canvas id="storage-canvas" class="hidden"></canvas>
             </div>
