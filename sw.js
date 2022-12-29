75d7627b105401bf2e77d6fd1d66a0030d98e9e9
JameFLC <jame.flochlecarour@gmail.com>
2022-12-29 22:40:27 +0100

    Fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = `my-sample-app-cache-v1`;
+const CACHE_NAME = `texture-packer-cache`;
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
