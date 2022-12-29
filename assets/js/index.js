dd22ee54d49639d520281942db4647ebeed746c6
JameFLC <jame.flochlecarour@gmail.com>
2022-12-29 19:18:13 +0100

    Fixed light dark theme switching

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,21 @@
 "use strict";
-const body = document.getElementById("body");
 const swappTheme = function () {
+    const body = document.getElementById("body");
     if (getTheme() == "dark")
         setTheme("light");
     else
         setTheme("dark");
 };
 const getTheme = function () {
+    const body = document.getElementById("body");
     return body.getAttribute("theme") || "light";
 };
 const setTheme = function (theme) {
+    const body = document.getElementById("body");
     body.setAttribute("theme", theme);
     localStorage.setItem("theme", theme);
 };
+// Swapp theme when pressing alt+t
 document.addEventListener("keydown", (e) => {
     if (e.key.toLowerCase() === "t" && e.altKey) {
         swappTheme();
--- sources/ts/index.ts
+++ sources/ts/index.ts
@@ -1,19 +1,21 @@
-const body = document.getElementById("body") as HTMLBodyElement;
-
 const swappTheme = function (): void {
+  const body = document.getElementById("body") as HTMLBodyElement;
   if (getTheme() == "dark") setTheme("light");
   else setTheme("dark");
 };
 
 const getTheme = function () {
+  const body = document.getElementById("body") as HTMLBodyElement;
   return body.getAttribute("theme") || "light";
 };
 
 const setTheme = function (theme: string): void {
+  const body = document.getElementById("body") as HTMLBodyElement;
   body.setAttribute("theme", theme);
   localStorage.setItem("theme", theme);
 };
 
+// Swapp theme when pressing alt+t
 document.addEventListener("keydown", (e) => {
   if (e.key.toLowerCase() === "t" && e.altKey) {
     swappTheme();
